93e86ea8bb7db51347561bc15e014d9f14cc05df
Josh Zoellner <Josh.Zoellner@gmail.com>
2020-08-06 17:39:07 -0400

    finished search feature

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -37,7 +37,9 @@ public class SearchController {
             model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
         }
         model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", columnChoices);
 
         return "search";
     }
+
 }
